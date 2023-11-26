c0be38e55609318ac765f1ef154e5f4514778b31
Oskar Puchalski <oskarpuchalski17@gmail.com>
2023-11-26 20:40:29 +0100

    fix: fixed issue with creating files in non-existing directory

--- scripts/create-template.js
+++ scripts/create-template.js
@@ -1,13 +1,23 @@
 import fs from 'fs';
 import path from 'path';
 
+const currentModuleURL = import.meta.url;
+const currentModulePath = path.dirname(new URL(currentModuleURL).pathname);
+
+const mainDirectory = path.resolve(currentModulePath, '..');
+
+const tasksDirectory = path.join(mainDirectory, 'tasks');
+
 const today = new Date();
 const year = today.getFullYear();
 const month = String(today.getMonth() + 1).padStart(2, '0');
 const day = String(today.getDate()).padStart(2, '0');
 const folderName = `${year}-${month}-${day}`;
+const folderPath = path.join(tasksDirectory, folderName);
 
-const folderPath = path.join('tasks', folderName);
+if (!fs.existsSync(tasksDirectory)) {
+    fs.mkdirSync(tasksDirectory);
+}
 
 if (!fs.existsSync(folderPath)) {
     fs.mkdirSync(folderPath);
@@ -17,8 +27,7 @@ if (!fs.existsSync(folderPath)) {
     const testFilePath = path.join(folderPath, 'index.test.ts');
     fs.writeFileSync(testFilePath, '// Tutaj skopiuj testy dla zadania. Uruchom je poleceniem `npm test`');
 
-    console.log(`Przygotowano szablon na zadanie w folderze tasks/${folderName} 🎄`)
+    console.log(`Przygotowano szablon na zadanie w folderze tasks/${folderName} 🎄`);
 } else {
     console.log(`Folder na dzisiejsze zadania już istnieje (tasks/${folderName}) 🤔`);
 }
-
