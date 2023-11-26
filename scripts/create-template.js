a5fb06d79f94856401c21a919df65d9615097384
psmyrdek <przemek.smyrdek@gmail.com>
2023-11-26 16:39:36 +0100

    feat: generate template for today's date

---
+++ scripts/create-template.js
@@ -0,0 +1,24 @@
+import fs from 'fs';
+import path from 'path';
+
+const today = new Date();
+const year = today.getFullYear();
+const month = String(today.getMonth() + 1).padStart(2, '0');
+const day = String(today.getDate()).padStart(2, '0');
+const folderName = `${year}-${month}-${day}`;
+
+const folderPath = path.join('tasks', folderName);
+
+if (!fs.existsSync(folderPath)) {
+    fs.mkdirSync(folderPath);
+    const indexFilePath = path.join(folderPath, 'index.ts');
+    fs.writeFileSync(indexFilePath, '// Tutaj skopiuj kod zadania');
+
+    const testFilePath = path.join(folderPath, 'index.test.ts');
+    fs.writeFileSync(testFilePath, '// Tutaj skopiuj testy dla zadania. Uruchom je poleceniem `npm test`');
+
+    console.log(`Przygotowano szablon na zadanie w folderze tasks/${folderName} 🎄`)
+} else {
+    console.log(`Folder na dzisiejsze zadania już istnieje (tasks/${folderName}) 🤔`);
+}
+
