b99a26eeb0f0dcb15949061dfeb7d24d46813269
Szymon Taranczewski <sz.taranczewski@gmail.com>
2023-12-05 09:44:27 +0100

    wip

--- tasks/2023-12-05/index.ts
+++ tasks/2023-12-05/index.ts
@@ -1,1 +1,22 @@
-// Tutaj skopiuj kod zadania
+export class ChristmasEmitter {
+    private events: Record<string, Function[]> = {}
+
+    on(event: string, callback: Function) {
+        if (!this.events[event]) {
+            this.events[event] = [];
+        }
+        this.events[event].push(callback);
+    }
+
+    off(event: string, callback: Function) {
+        if(this.events[event]) {
+            this.events[event] = this.events[event].filter(cb => cb !== callback);
+        }
+    }
+
+    emit(event: string) {
+        if(this.events[event]) {
+            this.events[event].forEach(callback => callback());
+        }
+    }
+}
