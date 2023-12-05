ef7e4800fb3bf01bc074355be8bbc6e99406b85e
Kacper Nadstoga <kapix007@gmail.com>
2023-12-05 11:03:36 +0100

    prepare types and interface for ChristmasEmitter class

--- tasks/2023-12-05/index.ts
+++ tasks/2023-12-05/index.ts
@@ -1,4 +1,31 @@
 // Tutaj skopiuj kod zadania
-export class ChristmasEmitter {
 
+type EventType = 'gift' | 'letter';
+
+type ChristmasHandler = () => void;
+
+type ChristmasEventList =   Map<EventType, ChristmasHandler[]>
+
+
+interface IChristmasEmitter {
+    christmasEvents: ChristmasEventList;
+    on: (eventName: EventType, callback: ChristmasHandler) => void;
+    off: (eventName: EventType, callback: ChristmasHandler) => void;
+    emit: (eventName: EventType) => void;
+}
+
+export class ChristmasEmitter implements IChristmasEmitter {
+    christmasEvents: ChristmasEventList;
+    constructor(){
+        this.christmasEvents = new Map<EventType, ChristmasHandler[]>();
+    }
+    on(eventName: EventType, callback: ChristmasHandler){
+
+    }
+    off(eventName: EventType, callback: ChristmasHandler){
+
+    }
+    emit(eventName: EventType){
+
+    }
 }
