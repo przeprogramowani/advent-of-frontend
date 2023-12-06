cbdda9a636f6d24a14a01a909cab54348ea505e8
Szymon Taranczewski <sz.taranczewski@gmail.com>
2023-12-06 10:35:14 +0100

    wip

--- tasks/2023-12-05/index.ts
+++ tasks/2023-12-05/index.ts
@@ -1,21 +1,22 @@
-export class ChristmasEmitter {
-    private events: Record<string, Function[]> = {}
+type Callback = () => void;
 
-    on(event: string, callback: Function) {
-        if (!this.events[event]) {
-            this.events[event] = [];
-        }
+export class ChristmasEmitter {
+    private events: Record<string, Callback[]> = {}
+
+
+    on(event: string, callback: Callback) {
+        this.events[event] = this.events[event] || [];
         this.events[event].push(callback);
     }
 
-    off(event: string, callback: Function) {
-        if(this.events[event]) {
+    off(event: string, callback: Callback) {
+        if (this.events[event]) {
             this.events[event] = this.events[event].filter(cb => cb !== callback);
         }
     }
 
     emit(event: string) {
-        if(this.events[event]) {
+        if (this.events[event]) {
             this.events[event].forEach(callback => callback());
         }
     }
