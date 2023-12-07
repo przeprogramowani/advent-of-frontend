3b2783bd65ed510cc549a7765e24691637a92d22
lukaasz555 <lukasz.zegar555@gmail.com>
2023-12-07 23:01:48 +0100

    solution

--- tasks/2023-12-07/index.ts
+++ tasks/2023-12-07/index.ts
@@ -1,5 +1,15 @@
 type Letter = { [key: string]: number };
+type ChangeTracker = (key: string, value: number) => void;
 
-export function createTrackedLetter(): Letter {
-  return {} as Letter;
+export function createTrackedLetter(letter: Letter, cb: ChangeTracker): Letter {
+  const trackedLetter = new Proxy(letter, {
+    set: (target, key: string, value: number) => {
+      if (target[key] !== value) {
+        cb(key, value);
+      }
+      target[key] = value;
+      return true;
+    },
+  });
+  return trackedLetter;
 }
